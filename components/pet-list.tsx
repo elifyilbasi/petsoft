e5ce6edf73814b58c96147832145bab1a824e9f2
Elif Varli <elifyilbasi@gmail.com>
2026-02-24 19:22:12 +0100

    add grid layout and dashboard items

---
+++ components/pet-list.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+export default function PetList() {
+  return (
+    <ul className="bg-white border-b boder-black/[0.08]">
+      <li>
+        <button className="flex items-center h-17.5 w-full cursor-pointer px-5 text-base gap-3 hover:bg-[#EFF1F2] focus:bg-[#EFF1F2] transition">
+          <Image
+            src="https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png"
+            alt="Pet image"
+            width={45}
+            height={45}
+            className="rounded-full object-cover"
+          />
+          <p className="font-semibold">Benjamin</p>
+        </button>
+      </li>
+    </ul>
+  );
+}
